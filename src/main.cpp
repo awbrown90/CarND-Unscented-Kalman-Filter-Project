3ec5bb2d8d77196ab340a3c3542d8a8149ec3dac
awbrown90 <aaronwbrown@msn.com>
2017-03-23 19:55:49 -0600

    Saving first working version of ukf

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include "Eigen/Dense"
 #include <vector>
 #include "ukf.h"
+#include "tools.h"
 #include "measurement_package.h"
+#include "ground_truth_package.h"
 #include <fstream>
 #include <sstream>
 #include <stdlib.h>
@@ -61,11 +63,25 @@ int main(int argc, char* argv[]) {
 
   check_files(in_file_, in_file_name_, out_file_, out_file_name_);
 
+  /***** Set up NIS tracking *********
+  * 	
+  ************************************/
+
+  // NIS 95% line based on df measurement
+  float NIS_R_95 = 7.815;
+  float NIS_L_95 = 5.991;
+
+  // count how many measurments are below 95% line
+  int R_below_95 = 0;
+  int L_below_95 = 0;
+
   /**********************************************
    *  Set Measurements                          *
    **********************************************/
 
   vector<MeasurementPackage> measurement_pack_list;
+  vector<GroundTruthPackage> gt_pack_list;
+
   string line;
 
   // prep the measurement packages (each line represents a measurement at a
@@ -73,8 +89,9 @@ int main(int argc, char* argv[]) {
   while (getline(in_file_, line)) {
     string sensor_type;
     MeasurementPackage meas_package;
+    GroundTruthPackage gt_package;
     istringstream iss(line);
-    long timestamp;
+    long long timestamp;
 
     // reads first element from the current line
     iss >> sensor_type;
@@ -110,17 +127,36 @@ int main(int argc, char* argv[]) {
       meas_package.timestamp_ = timestamp;
       measurement_pack_list.push_back(meas_package);
     }
+
+    // read ground truth data to compare later
+    float x_gt;
+    float y_gt;
+    float vx_gt;
+    float vy_gt;
+    iss >> x_gt;
+    iss >> y_gt;
+    iss >> vx_gt;
+    iss >> vy_gt;
+    gt_package.gt_values_ = VectorXd(4);
+    gt_package.gt_values_ << x_gt, y_gt, vx_gt, vy_gt;
+    gt_pack_list.push_back(gt_package);
   }
 
   // Create a UKF instance
   UKF ukf;
 
+  // used to compute the RMSE later
+  vector<VectorXd> estimations;
+  vector<VectorXd> ground_truth;
+
   size_t number_of_measurements = measurement_pack_list.size();
 
   // start filtering from the second frame (the speed is unknown in the first
   // frame)
+
   for (size_t k = 0; k < number_of_measurements; ++k) {
     // Call the UKF-based fusion
+  	
     ukf.ProcessMeasurement(measurement_pack_list[k]);
 
     // output the estimation
@@ -147,8 +183,53 @@ int main(int argc, char* argv[]) {
       out_file_ << ro * sin(phi) << "\t"; // p2_meas
     }
 
-    out_file_ << "\n";
+    // output the ground truth packages
+    out_file_ << gt_pack_list[k].gt_values_(0) << "\t";
+    out_file_ << gt_pack_list[k].gt_values_(1) << "\t";
+    out_file_ << gt_pack_list[k].gt_values_(2) << "\t";
+    out_file_ << gt_pack_list[k].gt_values_(3) << "\t";
+    out_file_ << ukf.NIS_laser_ << "\t";
+    out_file_ << ukf.NIS_radar_ << "\t";
+    out_file_ << k << "\n"; // counter value
+
+    if(ukf.NIS_laser_ < NIS_L_95)
+    {
+    	L_below_95++;
+    }
+    if(ukf.NIS_radar_ < NIS_R_95)
+    {
+    	R_below_95++;
+    }
+
+    VectorXd estimate(4);
+
+    double p_x = ukf.x_(0);
+    double p_y = ukf.x_(1);
+    double v  = ukf.x_(2);
+    double yaw = ukf.x_(3);
+
+    double v1 = cos(yaw)*v;
+    double v2 = sin(yaw)*v;
+
+    estimate(0) = p_x;
+    estimate(1) = p_y;
+    estimate(2) = v1;
+    estimate(3) = v2;
+
+    estimations.push_back(estimate);
+    ground_truth.push_back(gt_pack_list[k].gt_values_);
+	
   }
+
+  // compute the accuracy (RMSE)
+  Tools tools;
+  cout << "Accuracy - RMSE:" << endl << tools.CalculateRMSE(estimations, ground_truth) << endl;
+
+  float L_performance = (float)(L_below_95)/number_of_measurements;
+  float R_performance = (float)(R_below_95)/number_of_measurements;
+
+  cout << "NIS Laser Percent " << L_performance << endl;
+  cout << "NIS Radar Percent " << R_performance << endl;
 
   // close files
   if (out_file_.is_open()) {
@@ -159,6 +240,5 @@ int main(int argc, char* argv[]) {
     in_file_.close();
   }
 
-  cout << "Done!" << endl;
   return 0;
 }
